eaf68032cfff41da6b8843982969879034eb4ea7
Mukola Kovalyshyn <m.kovalushun@gmail.com>
2023-07-16 12:21:54 +0300

    Updates

---
+++ precache-manifest.bf5c6d0c6c25b273219950912393030d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d18a6c4ca00be03cb3daaf4c7ac9c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94901c9600289b7e683c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "973aec14cf70b5442531",
+    "url": "/static/js/2.cd790904.chunk.js"
+  },
+  {
+    "revision": "94901c9600289b7e683c",
+    "url": "/static/js/main.9840128d.chunk.js"
+  },
+  {
+    "revision": "878555ab93c92974de5d",
+    "url": "/static/js/runtime-main.037e578f.js"
+  }
+]);
